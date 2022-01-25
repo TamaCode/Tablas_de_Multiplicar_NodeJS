e42ec0d57025e5c9c17cf286970390382e10e0fc
Martin Tamareu <mg.tamareu@gmail.com>
2022-01-25 20:34:33 -0300

    Cambio mensaje de creacion exitosa

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ require('colors');
 console.clear();
 
 crearArchivoTabla(argv.b, argv.l, argv.m).then((fileName) => {
-  console.log(`${fileName} ha sido creado exitosamente.`.cyan);
+  console.log(`${fileName} ha sido creado exitosamente!`.cyan);
 }).catch((err)=> {
   console.log(err);
 });
